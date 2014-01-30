774391bf119eb28a8fc138dd93db2c8613d19bf7
André Baaij <andrebaaij@gmail.com>
2014-01-30 17:20:04 +0100

    Added some functions to the infrastructure of objects and commons
    Added two new interface functions to objects:
    - objects.create(objectName, x, y)
    - objects.list()

--- assets/javascript/common.js
+++ assets/javascript/common.js
@@ -7,6 +7,13 @@
 
 /* global Image,document,window,setTimeout,console,XMLHttpRequest */
 
+
+/*
+
+    Technical functions
+
+*/
+
 var common = {};
 
 common.getJSONFromURI = function(URI) {
@@ -28,3 +35,71 @@ common.getJSONFromURI = function(URI) {
     }
 
 };
+
+/*
+
+    Resources
+
+*/
+
+function resources(){}
+common.resources = new resources();
+
+function tilesets(){}
+common.resources.tilesets = new tilesets();
+
+
+var tileset = Image;
+tileset.prototype.grid = {
+    width : null,
+    height : null
+};
+tileset.prototype.nbErrors = 0;
+tileset.prototype.animations = null;
+
+tilesets.prototype.add = function(name) {
+    if (typeof common.resources.tilesets[name] !== 'undefined') {
+        console.log("Tileset " + name + " has already been loaded");
+        return null;
+    }
+    
+    var URI = './assets/images/tilesets/' + name + '.json';
+    
+    var tilesetObject = common.getJSONFromURI(URI);
+    
+    if (tileset === null) {
+        console.log("tilesets.add could not load " + URI);
+        return null;    
+    }
+    
+    var imageURI = './assets/images/tilesets/' + tilesetObject.image;
+    common.resources.tilesets[name] = new tileset();
+    common.resources.tilesets[name].addEventListener('load',function(){
+        this.nbErrors = 0;
+    });
+    
+    common.resources.tilesets[name].addEventListener('error',function(){
+        if(this.nbErrors <= 3) {
+            this.nbErrors += 1;
+            this.src = this.src;
+        } else {
+            console.error("tilesets.add could not load image " + imageURI);
+        }
+    });
+    
+    common.resources.tilesets[name].grid = tilesetObject.grid;
+    common.resources.tilesets[name].animations = tilesetObject.animations;
+    common.resources.tilesets[name].src = imageURI;
+    
+    return common.resources.tilesets[name];
+};
+
+tilesets.prototype.get = function(name) {
+    if(common.resources.tilesets[name] === undefined) {
+        return common.resources.tilesets.add(name);
+    }
+    
+    return common.resources.tilesets[name];
+};
+
+
--- assets/javascript/objects.js
+++ assets/javascript/objects.js
@@ -4,24 +4,73 @@
     Styleguide: https://github.com/airbnb/javascript
 */
 
+/*
+
+    Interface to objects:
+    
+    objects.create(objectName, x, y);
+    returns the newly created object;
+    
+    objects.list(orderArguments);
+    returns a list of all the objects sorted by arguments
+*/
+
 
 /* global Image,document,window,setTimeout,console,XMLHttpRequest,common */
 
-var objects = {};
+function objects() {}
+
+objects.prototype.list = function() {
+    return this.array;
+};
 
 function repository() {}
-
-repository.prototype.add = function(name) {
+repository.prototype.get = function(name) {
+    if (typeof objects.repository[name] !== 'undefined') {
+        console.log("Object " + name + " is already available!");
+        return objects.repository[name];
+    }
     var URI = './assets/objects/' + name + '.json';
     
     var object = common.getJSONFromURI(URI);
     
     if (object !== null) {
-        objects[name] = object;
+        objects.repository[name] = object;
+        objects.repository[name].tileset = common.resources.tilesets.get(object.tileset);
+        objects.repository[name].clone = function() {
+            return object;
+        };
+        objects.repository[name].create = function(x,y) {
+            var object = objects.repository[name].clone();
+            
+            // remove pure repository functions
+            delete object.clone;
+            delete object.create;
+            object.x = x;
+            object.y = y;
+            objects.add(object);
+        };
     } else {
         console.error("objects.loadObject could not load " + URI);
+        return null;
     }
+    
+    return objects.repository[name];
 };
 
+objects.prototype.create = function(name, x, y) {
+    var object = repository.prototype.get(name);
+    object.create(x,y);
+};
 
-objects.repository = new repository();
+objects.prototype.add = function(object) {
+    this.array.push(object);
+};
+
+// Initialise the Object objects.prototype.repository
+objects.prototype.repository = new repository();
+
+
+// Initialise the Object objects
+objects = new objects();
+objects.array= [];
