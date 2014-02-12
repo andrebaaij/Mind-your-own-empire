859521009cd1e2194e7a34bf30232bfb5faa7d13
J0h4n <j0h4nmail@gmail.com>
2014-02-12 20:57:16 +0100

    Draw update
    Draws ground layer

--- assets/javascript/draw.js
+++ assets/javascript/draw.js
@@ -5,105 +5,41 @@
 var draw = function(canvas, level, objects, xOffset, yOffset) {
     
     var tileset = level.tilesets[0];
+    
+    var tilewidth = tileset.tilewidth;
+    var tileheight = tileset.tileheight;
+    var imagewidth = tileset.imagewidth;
+    var imageheight = tileset.imageheight;
+    
+    var tilesPerRow = imagewidth / tilewidth;
+    
     var plaatje = common.resources.tilesets.get(tileset.name);
-    var sx = 128;
-    var sy = 0;
-    
+
     canvas.context = canvas.getContext("2d");
-    canvas.width = canvas.width + 20;
-    canvas.height = canvas.height + 10;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    var layer = level.layers[0];
+    for (var i = 0 ; i < imagewidth*imageheight ; i++) {
+        var tileIndex = layer.data[i] - 1;
+        var sx = tileIndex % tilesPerRow;
+        var sy = (tileIndex - sx) / tilesPerRow;
+        var cx = i % layer.width;
+        var cy = (i - cx) / layer.width;
+        canvas.context.drawImage(plaatje,
+                               sx * tilewidth,
+                               sy * tileheight,
+                               tilewidth,
+                               tileheight,
+                               0.5*(cx-cy-1)*tilewidth+0.5*window.innerWidth,
+                               0.5*(cx+cy)*tileheight,
+                               tilewidth,
+                               tileheight
+                            );
+    }
     
     
-    canvas.context.drawImage(  plaatje,
-                               sx,
-                               sy,
-                               tileset.tilewidth,
-                               tileset.tileheight,
-                               -64,
-                               -32,
-                               tileset.tilewidth,
-                               tileset.tileheight
-                            );
-    canvas.context.drawImage(  plaatje,
-                               sx+128,
-                               sy,
-                               tileset.tilewidth,
-                               tileset.tileheight,
-                               0,
-                               0,
-                               tileset.tilewidth,
-                               tileset.tileheight
-                            );
-    canvas.context.drawImage(  plaatje,
-                               sx,
-                               sy,
-                               tileset.tilewidth,
-                               tileset.tileheight,
-                               64,
-                               -32,
-                               tileset.tilewidth,
-                               tileset.tileheight
-                            );
-    canvas.context.drawImage(  plaatje,
-                               sx,
-                               sy,
-                               tileset.tilewidth,
-                               tileset.tileheight,
-                               128,
-                               0,
-                               tileset.tilewidth,
-                               tileset.tileheight
-                            );
-    canvas.context.drawImage(  plaatje,
-                               sx,
-                               sy,
-                               tileset.tilewidth,
-                               tileset.tileheight,
-                               64,
-                               32,
-                               tileset.tilewidth,
-                               tileset.tileheight
-                            );
-    canvas.context.drawImage(  plaatje,
-                               sx,
-                               sy,
-                               tileset.tilewidth,
-                               tileset.tileheight,
-                               192,
-                               32,
-                               tileset.tilewidth,
-                               tileset.tileheight
-                            );
-    canvas.context.drawImage(  plaatje,
-                               sx,
-                               sy,
-                               tileset.tilewidth,
-                               tileset.tileheight,
-                               128,
-                               64,
-                               tileset.tilewidth,
-                               tileset.tileheight
-                            );
-    canvas.context.drawImage(  plaatje,
-                               sx,
-                               sy,
-                               tileset.tilewidth,
-                               tileset.tileheight,
-                               0,
-                               64,
-                               tileset.tilewidth,
-                               tileset.tileheight
-                            );
-    canvas.context.drawImage(  plaatje,
-                               sx,
-                               sy,
-                               tileset.tilewidth,
-                               tileset.tileheight,
-                               64,
-                               96,
-                               tileset.tilewidth,
-                               tileset.tileheight
-                            );
+    
 
 
 };
