29fb0661a3db4d32d6855b4eb71366c6de15bf9f
André Baaij <andrebaaij@gmail.com>
2014-02-13 21:38:14 +0100

    Added resize function.

--- assets/javascript/userInterface.js
+++ assets/javascript/userInterface.js
@@ -5,8 +5,10 @@ userInterface.elements = {};
 userInterface.initialise = function() {
     userInterface.elements.canvas = document.getElementById("canvas");
     userInterface.elements.canvas.context = canvas.getContext("2d");
+    window.onresize();
+};
+
+window.onresize = function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 };
-
-//window.onresize
