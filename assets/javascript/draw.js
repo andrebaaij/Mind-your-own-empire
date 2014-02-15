c6f44b945018c3b0df09c8677f4d837d8716b66b
J0h4n <j0h4nmail@gmail.com>
2014-02-15 13:48:36 +0100

    Extended draw function
    Not working completely yet

--- assets/javascript/draw.js
+++ assets/javascript/draw.js
@@ -15,23 +15,31 @@ var draw = function(canvas, level, objects, xOffset, yOffset) {
     
     var plaatje = common.resources.tilesets.get(tileset.name);
     
-    var layer = level.layers[0];
-    for (var i = 0 ; i < imagewidth*imageheight ; i++) {
-        var tileIndex = layer.data[i] - 1;
-        var sx = tileIndex % tilesPerRow;
-        var sy = (tileIndex - sx) / tilesPerRow;
-        var cx = i % layer.width;
-        var cy = (i - cx) / layer.width;
-        canvas.context.drawImage(plaatje,
-                               sx * tilewidth,
-                               sy * tileheight,
-                               tilewidth,
-                               tileheight,
-                               0.5*(cx-cy-1)*tilewidth+0.5*window.innerWidth,
-                               0.5*(cx+cy)*tileheight,
-                               tilewidth,
-                               tileheight
-                            );
-    }
+    var numberOfTilesForHeight = Math.ceil(canvas.height/tileheight) * 2;
+    var numberOfTilesForWidth = Math.ceil(canvas.width/tilewidth) * 2 + 2;
     
+    var layer = level.layers[0];
+    for (var k = 0 ; k < numberOfTilesForHeight ; k++) {
+        for (var j = k-1 ; j < numberOfTilesForWidth + 5 ; j++) {
+            var i = k*level.width + j - 5;
+            if (i < 0) {
+                continue;
+            }
+            var tileIndex = layer.data[i] - 1;
+            var sx = tileIndex % tilesPerRow;
+            var sy = (tileIndex - sx) / tilesPerRow;
+            var cx = i % layer.width;
+            var cy = (i - cx) / layer.width;
+            canvas.context.drawImage(plaatje,
+                                   sx * tilewidth,
+                                   sy * tileheight,
+                                   tilewidth,
+                                   tileheight,
+                                   0.5*(cx-cy+5)*tilewidth,
+                                   0.5*(cx+cy)*tileheight,
+                                   tilewidth,
+                                   tileheight
+                                );
+        }
+    }
 };
