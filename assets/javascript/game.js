0added999f89dedc0ba435de94333b40cc21eee9
André Baaij <andrebaaij@gmail.com>
2014-04-24 07:48:58 +0200

    Added a few trees to the level

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,14 @@ var game = {};
 game.initialise = function() {
     objects.create("indianMale1",10,10);
     objects.create("firTree1",50,50);
+    objects.create("firTree1",100,50);
+    objects.create("firTree1",100,100);
+    objects.create("firTree1",150,50);
+    objects.create("firTree1",150,100);
+    objects.create("firTree1",150,150);
+    
+    
+    
     
     game.gameLoop();
 };
