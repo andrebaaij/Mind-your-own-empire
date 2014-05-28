ecd88ae3db73df8723cc4aaaeecee12cbd6a3c40
André Baaij <andrebaaij@gmail.com>
2014-05-28 23:30:59 +0200

    blocks are the new indians...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,14 +9,16 @@ game.initialise = function() {
     objects.repository.get("log");
     objects.repository.get("totemPole");
     
-    objects.create("indianMale",110,110);
-    objects.create("tree",150,150);
-    objects.create("tree",200,150);
-    objects.create("tree",200,200);
-    objects.create("tree",250,150);
-    objects.create("tree",250,200);
-    objects.create("tree",250,250);
-
+    //objects.create("indianMale",110,110);
+    //objects.create("tree",150,150);
+    //objects.create("tree",200,150);
+    //objects.create("tree",200,200);
+    //objects.create("tree",250,150);
+    //objects.create("tree",250,200);
+    //objects.create("tree",250,250);
+    
+    objects.create("block",250,250);
+    
     game.gameLoop();
 };
 
