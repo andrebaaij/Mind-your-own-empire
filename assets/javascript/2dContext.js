018f64fcebd9755d3b8bda12308f79c0c93661d0
André Baaij <andrebaaij@gmail.com>
2014-10-18 22:28:34 +0200

    context2d save

--- assets/javascript/2dContext.js
+++ assets/javascript/2dContext.js
@@ -18,7 +18,8 @@ function context2d(canvas) {
     
     this.initShaders();
     this.initBuffers();
-    this.initTexture();
+    var img = common.resources.tilesets.get("block");
+    this.initTexture(img);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
@@ -56,21 +57,101 @@ context2d.prototype.initShaders = function() {
 
 
 
-context2d.prototype.initTexture = function() {
-    neheTexture = gl.createTexture();
-    neheTexture.image = new Image();
-    neheTexture.image.onload = function () {
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, neheTexture);
-       // gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, neheTexture.image);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        //gl.bindTexture(gl.TEXTURE_2D, null);
-        
-    }.bind(this);
+context2d.prototype.initTexture = function(img) {
+//    neheTexture = gl.createTexture();
+//    neheTexture.image = new Image();
+//    neheTexture.image.onload = function () {
+//        gl.activeTexture(gl.TEXTURE0);
+//        gl.bindTexture(gl.TEXTURE_2D, neheTexture);
+//       // gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+//        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, neheTexture.image);
+//        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+//        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+//        //gl.bindTexture(gl.TEXTURE_2D, null);
+//        
+//    }.bind(this);
+//
+//    neheTexture.image.src = "./assets/nehe.gif";
+    
+//    if (img.buffer) {
+//        return;
+//    }
 
-    neheTexture.image.src = "./assets/nehe.gif";
+    var n2size = Math.pow(2, Math.ceil(Math.log(Math.max(img.width, img.height)) / Math.log(2)));
+    var n2img;
+
+    n2canvas = document.createElement('canvas');
+    n2canvas.width = n2canvas.height = n2size;
+    n2canvas.context = n2canvas.getContext('2d');
+    n2canvas.context.drawImage(img, 0, 0);
+
+    var buffer = {};
+
+    neheTexture = buffer.texture = gl.createTexture();
+    buffer.texture.image = n2canvas;
+
+    gl.bindTexture(gl.TEXTURE_2D, buffer.texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+
+    buffer.position = gl.createBuffer(); // imagePositionBuffer;
+
+    //    buffer.position.height = 500;
+    //    buffer.position.width = 500
+    //    buffer.position.itemSize = 3;
+    //    buffer.position.numItems = 500*500*3;
+    //    buffer.position.vertices = [];
+
+    //    for(var i = 0; i < buffer.position.numItems; i++) {
+    //        // push x
+    //        buffer.position.vertices.push(i%buffer.position.width)
+    //        
+    //        // push y
+    //        buffer.position.vertices.push(Math.floor(i/buffer.position.width))
+    //        
+    //        // push z
+    //        buffer.position.vertices.push(0);
+    //    }
+    //    
+    //    gl.bindBuffer(gl.ARRAY_BUFFER, buffer.position);
+    //    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(buffer.position.vertices), gl.STATIC_DRAW);
+    //    gl.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, buffer.position.itemSize, gl.FLOAT, false, 0, 0);
+
+
+
+    buffer.vertexTextureCoord = gl.createBuffer(); //imageVertexTextureCoordBuffer;
+    buffer.vertexTextureCoord.itemSize = 2;
+    buffer.vertexTextureCoord.numItems = 4;
+
+    buffer.vertexIndex = gl.createBuffer();
+    buffer.vertexIndex.itemSize = 1;
+    buffer.vertexIndex.numItems = 6;
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, buffer.texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, buffer.texture.image);
+
+    return img.buffer = buffer;
+
+//    this.ACTIVE_MAX_COMBINED_TEXTURE_IMAGE_UNITS += 1;
+//    if (this.ACTIVE_MAX_COMBINED_TEXTURE_IMAGE_UNITS === this.MAX_COMBINED_TEXTURE_IMAGE_UNITS) {
+//        this.ACTIVE_MAX_COMBINED_TEXTURE_IMAGE_UNITS = 0;
+//    }
+//
+//    var glActiveTextureReference = 'TEXTURE' + this.ACTIVE_MAX_COMBINED_TEXTURE_IMAGE_UNITS
+//
+//    gl.activeTexture(gl[glActiveTextureReference]);
+//    gl.bindTexture(gl.TEXTURE_2D, texture);
+//    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+//
+//    return texture.glActiveTextureReference = glActiveTextureReference;
+    
+    
+    
+    
+    
 }
 
 context2d.prototype.initBuffers = function() {
@@ -123,6 +204,16 @@ context2d.prototype.tick = function() {
 }
 
 context2d.prototype.drawScene = function() {
+    var img = common.resources.tilesets.get("block");
+    this.drawImage(img);
+    this.initTexture(img);
+    
+    
+    
+    
+    
+    
+    
     //gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -175,12 +266,133 @@ context2d.prototype.getShader = function(gl, id) {
     return shader;
 }
 
-context2d.prototype.handleLoadedTexture = function(texture) {
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-   // gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    //gl.bindTexture(gl.TEXTURE_2D, null);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+    Extra
+
+*/
+
+
+context2d.prototype.drawImage = function () {
+    var params = {};
+
+    // Default settings
+    params.img = arguments[0];
+    // If the image is not loaded yet, we can stop this.
+    if (!params.img || !params.img.complete) {
+        return;
+    }
+
+//    params.width = params.img.width;
+//    params.height = params.img.height;
+//    params.sx = 0;
+//    params.sy = 0;
+//    params.swidth = params.img.width;
+//    params.sheight = params.img.height;
+//
+//    if (arguments.length === 3) {
+//        // context.drawImage(img,x,y);
+//        params.x = arguments[1];
+//        params.y = arguments[2];
+//    } else if (arguments.length === 5) {
+//        // context.drawImage(img,x,y,width,height);
+//        params.x = arguments[1];
+//        params.y = arguments[2];
+//        params.width = arguments[3];
+//        params.height = arguments[4];
+//    } else if (arguments.length === 9) {
+//        // context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
+//        params.sx = arguments[1];
+//        params.sy = arguments[2];
+//        params.swidth = arguments[3];
+//        params.sheight = arguments[4];
+//        params.x = arguments[5];
+//        params.y = arguments[6];
+//        params.width = arguments[7];
+//        params.height = arguments[8];
+//    } else {
+//        // incorrect number of arguments.
+//        return;
+//    }
+//
+    // If the image is not buffered yet, buffer it now.
+    if (!params.img.buffer) {
+        params.img.buffer = this.bufferImage(params.img);
+    }
+//
+//    // If the texture is not activate anymore, activate it.
+//    if (!params.img.buffer.texture.glActiveTextureReference) {
+//        params.img.buffer.texture.glActiveTextureReference = this.activateTexture(params.img.buffer.texture);
+//    }
+//
+//    params.img.buffer.position.vertices = [
+//    50, 50, 1.0,
+//     0, 50, 1.0,
+//    50,  0, 1.0,
+//     0,  0, 1.0
+//    ];
+//
+//    params.img.buffer.textureCoords = [
+//        50, 50,
+//         0, 50,
+//         0, 0,
+//        50, 0
+//    ];
+//
+//    params.img.buffer.cubeVertexIndices = [
+//        0, 1, 2,
+//        0, 2, 3
+//    ];
+//
+//    // If the texturesize has changed, assign it again.
+//    if (params.img.buffer.texture.image.width !== this.resolutionSize) {
+//        gl.uniform2f(this.shaderProgram.textureresolutionLocation,
+//            params.img.buffer.texture.image.width,
+//            params.img.buffer.texture.image.height);
+//
+//        this.resolutionSize = params.img.buffer.texture.image.width;
+//    }
+//
+//    gl.bindBuffer(gl.ARRAY_BUFFER, params.img.buffer.position);
+//    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(params.img.buffer.position.vertices), gl.STATIC_DRAW);
+//    gl.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, params.img.buffer.position.itemSize, gl.FLOAT, false, 0, 0);
+//
+//    // look up where the vertex data needs to go.
+//    var positionLocation = gl.getAttribLocation(this.shaderProgram, "a_position");
+//    gl.enableVertexAttribArray(positionLocation);
+//    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+//
+//    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, params.img.buffer.vertexIndex);
+//    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(params.img.buffer.cubeVertexIndices), gl.STATIC_DRAW);
+//
+//    gl.bindBuffer(gl.ARRAY_BUFFER, params.img.buffer.vertexTextureCoord);
+//    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(params.img.buffer.textureCoords), gl.STREAM_DRAW);
+//    gl.vertexAttribPointer(this.shaderProgram.textureCoordAttribute, params.img.buffer.vertexTextureCoord.itemSize, gl.FLOAT, false, 0, 0);
+//
+//
+//    //gl.activeTexture(gl[params.img.buffer.texture.glActiveTextureReference]);
+//
+//    //gl.uniform1i(this.shaderProgram.samplerUniform, 0);
+//
+//    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, params.img.buffer.vertexIndex);
+//    this.setMatrixUniforms(gl);
+//    gl.drawElements(gl.TRIANGLES, params.img.buffer.vertexIndex.numItems, gl.UNSIGNED_SHORT, 0);
+};
